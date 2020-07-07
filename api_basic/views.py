4d7011d6197609dc3495b806d38b708b9f8545d2
Anjali Pushkar <anjalipushkar15@gmail.com>
2020-07-07 23:57:36 +0530

    views

--- api_basic/views.py
+++ api_basic/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Article
+from .serializers import ArticleSerializer
 
 # Create your views here.
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many = True)
+        return JsonResponse(serializer.data, safe = False)
