9175bef13282a21560e003596a47d0ba84a583bc
Anjali Pushkar <anjalipushkar15@gmail.com>
2020-07-10 02:41:10 +0530

    generic classes

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import article_list, article_detial, ArticleAPIView, ArticleDetails
+from .views import article_list, article_detial, ArticleAPIView, ArticleDetails, GenericAPIView
 
 urlpatterns = [
     # path('article/', article_list),
     path('article/', ArticleAPIView.as_view()),
     # path('detail/<int:pk>', article_detial),
     path('detail/<int:id>', ArticleDetails.as_view()),
+    path('generic/article/<int:id>', GenericAPIView.as_view()),
+
 ]
